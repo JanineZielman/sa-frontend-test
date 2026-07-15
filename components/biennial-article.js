b6cc32635c6f9d90780bfe6847fa97c684e821ae
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2026-07-15 20:19:01 +0200

    test url

--- components/biennial-article.js
+++ components/biennial-article.js
@@ -1,35 +1,35 @@
 import ReactMarkdown from "react-markdown";
-import React, {useEffect} from "react"
+import React, { useEffect } from "react"
 import Moment from 'moment';
 import Image from "./image"
 import LazyLoad from 'react-lazyload';
 import Collapsible from "./collapsible";
 
-const Article = ({page, relations, params}) => {
+const Article = ({ page, relations, params }) => {
 	useEffect(() => {
-    var text = document.getElementsByClassName('text-block');
-		for (let i = 0; i < text.length; i++) { 
+		var text = document.getElementsByClassName('text-block');
+		for (let i = 0; i < text.length; i++) {
 			var links = text[i].getElementsByTagName('a');
-			for (let j = 0; j < links.length; j++) { 
+			for (let j = 0; j < links.length; j++) {
 				if (links[j].href.includes('#footnotes') != true) {
 					links[j].setAttribute('target', '_blank');
 				} else {
 					links[j].classList.add('footnote')
 				}
 				if (links[j].href.includes('.pdf') == true) {
-					links[j].href = 'https://cms-dev.sonicacts.com/uploads/' + links[j].href.substring(links[j].href.lastIndexOf("/") + 1)
+					links[j].href = 'http://91.184.31.135:1338/uploads/' + links[j].href.substring(links[j].href.lastIndexOf("/") + 1)
 				}
 			}
 		}
-  }, []);
+	}, []);
 
-  return (   
+	return (
 		<section className="article biennial-article">
 			<>
 				{relations?.attributes?.authors?.data &&
 					<div className="tags">
 						{relations.attributes.authors.data.map((author, i) => {
-							return(
+							return (
 								<a className="author" href={`/biennial/${params.slug}/artists/${author.attributes.slug}`}>
 									{author.attributes.name}
 								</a>
@@ -40,11 +40,11 @@ const Article = ({page, relations, params}) => {
 				{page.attributes.title &&
 					<div className="title">
 						<h1>{page.attributes.title}</h1>
-						{relations.attributes.biennial_tags?.data && 
+						{relations.attributes.biennial_tags?.data &&
 							<div className="category">
 								{relations.attributes.biennial_tags.data.map((tag, i) => {
-									return(
-										<a href={'/search/'+tag.attributes.slug} key={'search'+i}>
+									return (
+										<a href={'/search/' + tag.attributes.slug} key={'search' + i}>
 											{tag.attributes.title}
 										</a>
 									)
@@ -62,70 +62,70 @@ const Article = ({page, relations, params}) => {
 				<div className="content">
 					<div className={`wrapper ${page.attributes.slug}`}>
 						<>
-						{page.attributes.content?.map((item, i) => {							
-							return (
-								<div key={`content${i}`} className={`${page.attributes.slug}-block`}>
-									{item.image?.data &&
-										<LazyLoad height={600}>
-											{item.image_caption ?
-												<div className="columns" key={'column'+i}>
-													<div className="caption">
-														<ReactMarkdown 
-															children={item.image_caption} 
+							{page.attributes.content?.map((item, i) => {
+								return (
+									<div key={`content${i}`} className={`${page.attributes.slug}-block`}>
+										{item.image?.data &&
+											<LazyLoad height={600}>
+												{item.image_caption ?
+													<div className="columns" key={'column' + i}>
+														<div className="caption">
+															<ReactMarkdown
+																children={item.image_caption}
+															/>
+														</div>
+														<div className={`image ${page.attributes.slug}`}>
+															<Image image={item.image.data.attributes} placeholder="blur" blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8VQ8AAnkBewKPWHQAAAAASUVORK5CYII=" />
+														</div>
+													</div>
+													:
+													<div className={`image ${item.size} ${page.attributes.slug}`}>
+														<Image image={item.image.data.attributes} placeholder="blur" blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8VQ8AAnkBewKPWHQAAAAASUVORK5CYII=" />
+													</div>
+												}
+											</LazyLoad>
+										}
+										{item.sidenote &&
+											<div className={'sidenote ' + item.size}>
+												<ReactMarkdown
+													children={item.sidenote}
+												/>
+											</div>
+										}
+										{item.text_block &&
+											<div className={'text-block ' + item.size} key={'text' + i}>
+
+												<ReactMarkdown
+													children={item.text_block}
+												/>
+											</div>
+										}
+										{item.url &&
+											<div className={`iframe-wrapper ${item.sound}`} key={'url' + i}>
+												<iframe className="iframe" src={item.url.match(/\bhttps?:\/\/\S+/gi)[0]} frameBorder="0" />
+											</div>
+										}
+										{item.__component == 'basic.collapsible' &&
+											<div className="collapsible about">
+												<Collapsible trigger={item.title} open={item.open == true && item.open}>
+													<div className={'text-block ' + item.text?.size} key={'textcol' + i}>
+														<ReactMarkdown
+															children={item.text?.text_block}
 														/>
 													</div>
-													<div className={`image ${page.attributes.slug}`}>
-														<Image image={item.image.data.attributes} placeholder="blur" blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8VQ8AAnkBewKPWHQAAAAASUVORK5CYII="/>
-													</div>
-												</div>
-												:
-												<div className={`image ${item.size} ${page.attributes.slug}`}>
-													<Image image={item.image.data.attributes} placeholder="blur" blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8VQ8AAnkBewKPWHQAAAAASUVORK5CYII="/>
-												</div>
-											}
-										</LazyLoad>
-									}
-									{item.sidenote && 
-										<div className={'sidenote ' + item.size}>
-											<ReactMarkdown 
-												children={item.sidenote} 
-											/>
-										</div>
-									}
-									{item.text_block &&
-										<div className={'text-block ' + item.size} key={'text'+i}>
-										
-											<ReactMarkdown 
-												children={item.text_block} 
-											/>
-										</div>
-									}
-									{item.url &&
-										<div className={`iframe-wrapper ${item.sound}`}  key={'url'+i}>
-											<iframe className="iframe" src={item.url.match(/\bhttps?:\/\/\S+/gi)[0]} frameBorder="0"/>
-										</div>
-									}
-									{item.__component == 'basic.collapsible' &&
-										<div className="collapsible about">
-											<Collapsible trigger={item.title} open={item.open == true && item.open}>
-												<div className={'text-block ' + item.text?.size} key={'textcol'+i}>
-													<ReactMarkdown 
-														children={item.text?.text_block} 
-													/>
-												</div>
-											</Collapsible>
-										</div>
-									}
+												</Collapsible>
+											</div>
+										}
+									</div>
+								)
+							})}
+							{relations?.attributes?.footnotes &&
+								<div className="footnotes" id="footnotes">
+									<ReactMarkdown
+										children={relations?.attributes?.footnotes?.footnotes}
+									/>
 								</div>
-							)
-						})}
-						{relations?.attributes?.footnotes &&
-							<div className="footnotes" id="footnotes">
-								<ReactMarkdown 
-									children={relations?.attributes?.footnotes?.footnotes} 
-								/>
-							</div>
-						}
+							}
 						</>
 					</div>
 
@@ -134,36 +134,36 @@ const Article = ({page, relations, params}) => {
 						{relations.attributes.WhenWhere?.[0] ?
 							<>
 								<span className="locations-sidebar">Location{relations.attributes.WhenWhere.length > 1 && 's'}</span>
-								{relations.attributes.WhenWhere.map((item,i) => {
-									return(
+								{relations.attributes.WhenWhere.map((item, i) => {
+									return (
 										<div className="sidebar-info">
 											{item.locations?.data?.map((loc) => {
-												return(
+												return (
 													<div className="date">
 														<div className="location">
 															<a href={`/biennial/${params.slug}/visit`}>
 																{loc.attributes.title} {loc.attributes.subtitle && <> – {loc.attributes.subtitle} </>}
 															</a>
-															
+
 														</div>
 													</div>
 												)
 											})}
 											{item.dates?.map((date) => {
-												return(
+												return (
 													<div className="date">
 														{Moment(date.start_date).format('D MMM')} {date.end_date && `– ${Moment(date.end_date).format('D MMM')}`}
 													</div>
 												)
 											})}
 											{item.times?.map((time) => {
-												return(
+												return (
 													<div className="date">
 														{time.start_time?.substring(0, 5)} {time.end_time && `– ${time.end_time?.substring(0, 5)}`}
 													</div>
 												)
 											})}
-											
+
 										</div>
 									)
 								})}
@@ -175,7 +175,7 @@ const Article = ({page, relations, params}) => {
 								}
 								{relations?.attributes?.start_date && relations?.attributes?.dates == null &&
 									<>
-									<div className="date">{Moment(relations.attributes.start_date).format('D MMM')} {relations.attributes.end_date && <>– {Moment(relations.attributes.end_date).format('D MMM')}</>}</div>
+										<div className="date">{Moment(relations.attributes.start_date).format('D MMM')} {relations.attributes.end_date && <>– {Moment(relations.attributes.end_date).format('D MMM')}</>}</div>
 									</>
 								}
 								{relations?.attributes?.dates?.[0] &&
@@ -186,19 +186,19 @@ const Article = ({page, relations, params}) => {
 											</div>
 										}
 										{relations?.attributes?.dates.map((date, i) => {
-											return(
+											return (
 												<div className="date" key={`dates-${i}`}>
 													{date.single_date &&
 														<div>
-														– {Moment(date.single_date).format('D MMM')}
+															– {Moment(date.single_date).format('D MMM')}
 														</div>
 													}
 													{date.end_date &&
 														<>
-														{relations?.attributes?.date &&
-															Moment(relations?.attributes?.start_date).format('D MMM')
-														}
-														&nbsp;– {Moment(date.end_date).format('D MMM')}
+															{relations?.attributes?.date &&
+																Moment(relations?.attributes?.start_date).format('D MMM')
+															}
+															&nbsp;– {Moment(date.end_date).format('D MMM')}
 														</>
 													}
 												</div>
@@ -214,7 +214,7 @@ const Article = ({page, relations, params}) => {
 											<>
 												{relations.attributes?.start_time?.substring(0, 5)} {relations.attributes.end_time && <>– {relations.attributes?.end_time?.substring(0, 5)}</>}
 												{relations.attributes?.times?.map((time, i) => {
-													return(
+													return (
 														<div>
 															{time.start_time?.substring(0, 5)} {time.end_time && `– ${time.end_time?.substring(0, 5)}`}
 														</div>
@@ -234,17 +234,17 @@ const Article = ({page, relations, params}) => {
 								}
 
 
-								{relations?.attributes?.locations?.data[0] && 
+								{relations?.attributes?.locations?.data[0] &&
 									<div>
 										<span>Location</span>
 										<div className="date">
 											{relations.attributes.locations?.data?.map((loc, j) => {
-												return(
+												return (
 													<div className="location">
 														<a href={`/biennial/${params.slug}/visit`}>
 															{loc.attributes.title} {loc.attributes.subtitle && <> – {loc.attributes.subtitle} </>}
 														</a>
-														
+
 													</div>
 												)
 											})}
@@ -258,9 +258,9 @@ const Article = ({page, relations, params}) => {
 							<a href={relations.attributes.ticket_link} className="sidebar-tickets">
 								<span>Tickets</span>
 								<div>
-									{relations.attributes.free ? 
-										`Free` 
-									:
+									{relations.attributes.free ?
+										`Free`
+										:
 										<>
 											{relations.attributes.price ? `€ ${relations.attributes.price}` : `Link to tickets`}
 										</>
@@ -277,7 +277,7 @@ const Article = ({page, relations, params}) => {
 								</div>
 							</a>
 						}
-						
+
 						{/* {relations?.attributes?.community_items?.data[0] &&
 							<div>
 								<span>Artists</span>
@@ -295,7 +295,7 @@ const Article = ({page, relations, params}) => {
 							<div className="program-side-wrapper">
 								<span>Programmes</span>
 								{relations.attributes.main_programmes.data.map((programme, i) => {
-									return(
+									return (
 										<div className="program-side">
 											<h2>{programme.attributes.title}</h2>
 											{programme.attributes.start_date &&
@@ -310,15 +310,15 @@ const Article = ({page, relations, params}) => {
 										</div>
 									)
 								})}
-								
+
 							</div>
 						}
-						
+
 					</div>
 				</div>
 			</>
 		</section>
-  )
+	)
 }
 
 
